6dce2b266c151a388150a84134f8404d9f1154c9
Alex Falkovskiy <alexander.falkovskiy@gmail.com>
2023-12-29 17:20:07 -0700

    Update animtest.py

--- animtest.py
+++ animtest.py
@@ -5,25 +5,25 @@ import numpy as np
 import math
 
 
-pi = math.pi
-g = 9.81
+# pi = math.pi
+# g = 9.81
 
-def projectile(v, alpha, dt=0.01):
-    vx = v * math.cos(alpha)
-    vy = v * math.sin(alpha)
-    g = 9.81
-    t1 = vy/g
-    t2 = 2*t1
-    d = vx*t2
-    h = vy*t1 - 0.5*g*t1**2
-    t2 = 2 * vy / g
-    t = np.linspace(0, t2, int(t2/dt))
-    x = vx * t
-    y = vy * t - 0.5 * g * np.square(t)
-    return x, y, t2, d, h
+# def projectile(v, alpha, dt=0.01):
+#     vx = v * math.cos(alpha)
+#     vy = v * math.sin(alpha)
+#     g = 9.81
+#     t1 = vy/g
+#     t2 = 2*t1
+#     d = vx*t2
+#     h = vy*t1 - 0.5*g*t1**2
+#     t2 = 2 * vy / g
+#     t = np.linspace(0, t2, int(t2/dt))
+#     x = vx * t
+#     y = vy * t - 0.5 * g * np.square(t)
+#     return x, y, t2, d, h
 
-progress_bar = st.sidebar.progress(0)
-status_text = st.sidebar.empty()
+# progress_bar = st.sidebar.progress(0)
+# status_text = st.sidebar.empty()
 
 st.header('test Projectile Motion \n AF, Dec 2023'
 
