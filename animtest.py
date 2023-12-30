7de216c0be5a1fe6dfcd7145401c5d0b1a2d0d82
Alex Falkovskiy <alexander.falkovskiy@gmail.com>
2023-12-29 17:22:39 -0700

    Update animtest.py

--- animtest.py
+++ animtest.py
@@ -27,22 +27,22 @@ import math
 
 st.header('test Projectile Motion \n AF, Dec 2023')
 
-# col1, col2 = st.columns(2)
-# with col1:
-#     v = st.slider('Velocity (m/s)', value=120., min_value=1., max_value=1500.)
-#     # st.write("Velocity = ", v)
-# with col2:
-#     alpha = st.slider('Inclination to the horizontal (deg)', value=30., min_value=0., max_value=90.)
-#     alpha_rad = pi * alpha / 180.
+col1, col2 = st.columns(2)
+with col1:
+    v = st.slider('Velocity (m/s)', value=120., min_value=1., max_value=1500.)
+    # st.write("Velocity = ", v)
+with col2:
+    alpha = st.slider('Inclination to the horizontal (deg)', value=30., min_value=0., max_value=90.)
+    alpha_rad = pi * alpha / 180.
 
-# chart = st.line_chart(np.zeros(shape=(1,1)))
-# x = np.arange(0, 100*np.pi, 0.1)
+chart = st.line_chart(np.zeros(shape=(1,1)))
+x = np.arange(0, 100*np.pi, 0.1)
 
-# for i in range(1, 101):
-#     y = np.sin(x[i])
-#     status_text.text("%i%% Complete" % i)
-#     chart.add_rows([y])
-#     progress_bar.progress(i)
-#     time.sleep(0.05)
+for i in range(1, 101):
+    y = np.sin(x[i])
+    status_text.text("%i%% Complete" % i)
+    chart.add_rows([y])
+    progress_bar.progress(i)
+    time.sleep(0.05)
 
-# progress_bar.empty()
+progress_bar.empty()
