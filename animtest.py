3c77dbe5d43c13459324235c7a74da992c286114
Alex Falkovskiy <alexander.falkovskiy@gmail.com>
2023-12-29 17:22:02 -0700

    Update animtest.py

--- animtest.py
+++ animtest.py
@@ -25,7 +25,7 @@ import math
 # progress_bar = st.sidebar.progress(0)
 # status_text = st.sidebar.empty()
 
-st.header('test Projectile Motion \n AF, Dec 2023'
+st.header('test Projectile Motion \n AF, Dec 2023')
 
 # col1, col2 = st.columns(2)
 # with col1:
